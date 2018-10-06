9a8ab5e7cf867757c7986ea5d7ecbc04c3732443
Lucas Passos Sperandio <lsperandio@filial.supriservice.local>
2018-10-06 10:27:45 -0300

    Alteração de código Pilha.

--- pilha.c
+++ pilha.c
@@ -11,16 +11,31 @@ struct pilha {
   No *primeiro;
 };
 
-Pilha* pilha_criar() {
-	//Implemente
+Pilha* pilha_criar(int tamanho) {
+	Pilha *p = (Pilha*) malloc(sizeof(Pilha));
+	p->topo = 0;
+	p->dados =(Token*) malloc(tamanho*sizeof(Token));
+	p->tamanho = tamanho;
+	return p;
 }
 
 void pilha_push(Pilha *p, Token t) {
-	//Implemente
+	if(p->topo >= p->tamanho) {
+		printf("Pilha Cheia!\n");
+		return;
+	}
+	p->dados[p->topo] = info;
+	p->topo++;
 }
 
 Token pilha_pop(Pilha *p) {
-	//Implemente
+	if(p->topo == 0) {
+		printf(Pilha Vazia"\n");
+		return 0;
+	}
+	p->topo--;
+	return p->dados[p->topo];
+
 }
 
 Token pilha_primeiro(Pilha *p) {
@@ -32,7 +47,8 @@ int pilha_vazia(Pilha *p) {
 }
 
 void pilha_destruir(Pilha *p) {
-	//Implemente
+	free(p->dados);
+	free(p);
 }
 
 void pilha_imprimir(Pilha *p) {
